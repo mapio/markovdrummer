fc48cce02f98128a57afefc092aeb1f0cb9eb51b
Massimo Santini <santini@di.unimi.it>
2014-03-05 23:36:03 +0100

    Fixes and refactoring

--- markovdrummer/midi/symbolic.py
+++ markovdrummer/midi/symbolic.py
@@ -5,87 +5,87 @@ from math import ceil
 import midi
 
 from ..midi.utils import track2eventdict, eventdict2track, is_noteon
+from ..midi.constants import GM10_PITCH_TO_DURMPART
 
-def events2pitch( events ):
-   return tuple( sorted(
-      map( lambda _: _.data[ 0 ], filter( is_noteon, events ) )
-   ) )
+def events2beat( events ):
+	return tuple( sorted(
+		map( lambda _: _.data[ 0 ], filter( is_noteon, events ) )
+	) )
 
-def sym2track( sym, tick, tick_off = None ):
-   if tick_off is None: tick_off = tick / 4
-   eventdict = dict()
-   for n, beat in enumerate( sym ):
-      print n, beat
-      events_at_tick = []
-      for note in beat:
-         events_at_tick.append( midi.NoteOnEvent( tick = 0, channel = 9, data = [ note, 64 ] ) )
-      if events_at_tick: eventdict[ n * tick ] = events_at_tick
-   return midi.Track( eventdict2track( eventdict, tick_off ) )
+def beat2events( beat ):
+	return [ midi.NoteOnEvent( tick = 0, channel = 9, data = [ pitch, 64 ] ) for pitch in beat ]
 
-def track2sym( track, quantize_resolution ):
+def beats2track( beats, tick, tick_off = None ):
+	if tick_off is None: tick_off = tick / 4
+	eventdict = dict()
+	for n, beat in enumerate( beats ):
+		events_at_tick = beat2events( beat )
+		if events_at_tick: eventdict[ n * tick ] = events_at_tick
+	return midi.Track( eventdict2track( eventdict, tick_off ) )
 
-   t2ed = track2eventdict( track )
-   tick_per_quantum = max( t2ed.keys() ) / quantize_resolution
-   res = []
+def track2beats( track, quantize_resolution ):
 
-   last_beat = 0
-   for tick, events in sorted( t2ed.items() ):
-      beat = int( ceil( tick / tick_per_quantum ) )
-      delta = beat - last_beat - 1
-      if delta: res.extend( [ tuple() ] * delta )
-      res.append( events2pitch( events ) )
-      last_beat = beat
+	t2ed = track2eventdict( track )
+	tick_per_quantum = max( t2ed.keys() ) / quantize_resolution
+	res = []
 
-   return res
+	last_beat = 0
+	for tick, events in sorted( t2ed.items() ):
+		beat = int( ceil( tick / tick_per_quantum ) )
+		delta = beat - last_beat - 1
+		if delta: res.extend( [ tuple() ] * delta )
+		res.append( events2beat( events ) )
+		last_beat = beat
 
-   TABLE_HTML = """
-<!DOCTYPE html>
-<head>
-   <meta charset="utf-8">
-  <title>Explain</title>
-  <style>
-    table, td, th {{
-      border: 1pt solid black;
-      border-collapse: collapse;
-      margin: 1em;
-    }}
-    td {{
-      width: 1em;
-    }}
-    .nob {{
-      border: none;
-    }}
-    .on {{
-      background-color: green;
-    }}
-    th {{
-      text-align: left;
-    }}
-  </style>
-</head>
-<body>
-{}
-</body>
-</html>
-"""
+	return res
 
-def explain( syms ):
-   def _explain( sym ):
-      notes = set()
-      for beat in sym:
-         notes.update( beat )
-      notes = sorted( notes, reverse = True )
-      table = [ '<table>' ]
-      table.append( ''.join(
-         [ '<th>&nbsp;' ] + [ '<th>{}'.format( n ) for n, beat in enumerate( sym, 1 ) ]
-      ) )
-      for note in notes:
-         table.append( ''.join(
-            [ '<tr><th>{}'.format( GM10_PITCH_TO_DURMPART[ note ] if note in GM10_PITCH_TO_DURMPART else note ) ]
-            + [ '<td class=on>&nbsp;' if note in beat else '<td>&nbsp;' for beat in sym ]
-          ) )
-      table.append( '</table>' )
-      return '\n'.join( table )
-   return TABLE_HTML.format( ' \n '.join( [ _explain( _ ) for _ in syms ] ) )
+def writetables( tables, path ):
+	TABLE_HTML = """
+	<!DOCTYPE html>
+	<head>
+	   <meta charset="utf-8">
+	  <title>Explain</title>
+	  <style>
+		table, td, th {{
+		  border: 1pt solid black;
+		  border-collapse: collapse;
+		  margin: 1em;
+		}}
+		td {{
+		  width: 1em;
+		}}
+		.nob {{
+		  border: none;
+		}}
+		.on {{
+		  background-color: green;
+		}}
+		th {{
+		  text-align: left;
+		}}
+	  </style>
+	</head>
+	<body>
+	{}
+	</body>
+	</html>
+	"""
+	with open( path, 'w' ) as f: f.write( TABLE_HTML.format( '\n'.join( tables ) ) )
 
+def beats2pitches( beats ):
+	pitches = set()
+	for beat in beats: pitches.update( beat )
+	return sorted( pitches, reverse = True )
 
+def beats2table( beats ):
+	table = [ '<table>' ]
+	table.append( ''.join(
+		[ '<th>&nbsp;' ] + [ '<th>{}'.format( n ) for n in range( 1, len( beats ) + 1 ) ]
+	) )
+	for pitch in beats2pitches( beats ):
+		table.append( ''.join(
+				[ '<tr><th>{}'.format( GM10_PITCH_TO_DURMPART[ pitch ] if pitch in GM10_PITCH_TO_DURMPART else pitch ) ]
+				+ [ '<td class=on>&nbsp;' if pitch in beat else '<td>&nbsp;' for beat in beats ]
+		) )
+	table.append( '</table>' )
+	return '\n'.join( table )
--- quantize.py
+++ quantize.py
@@ -3,7 +3,7 @@ from sys import argv
 from pprint import PrettyPrinter
 from json import dumps
 
-from markovdrummer.midi import clean, quantize, track2sym, sym2track
+from markovdrummer.midi import clean, quantize, track2beats, beats2track, beats2table, writetables
 from markovdrummer.markov import analyze, generate
 
 import midi
@@ -20,22 +20,20 @@ if __name__ == '__main__':
 
 	track = clean( original[ 1 ] if ntracks == 2 else original[ 0 ] )
 	qtrack = quantize( track, quantize_resoluzion )
+
 	qmidi = midi.Pattern(
 		format = original.format,
 		resolution = original.resolution,
 		tracks =  [ original[ 0 ], qtrack ] if ntracks == 2 else [ qtrack ]
 	)
-
 	midi.write_midifile( 'q_' + filename, qmidi )
 
-	sym = track2sym( qtrack, quantize_resoluzion )
-	#with  open( filename + '.html', 'w' ) as f: f.write( explain( [ sym ] ) )
-	model = analyze( sym )
-	pp( model )
-	gcart = generate( model, quantize_resoluzion * 4 )
-	gtrack = sym2track( gcart, 120 )
+	beats = track2beats( qtrack, quantize_resoluzion )
 
-	#with  open( 'g_' + filename + '.html', 'w' ) as f: f.write( explain( [ gcart ] ) )
+	model = analyze( beats )
+	pp( model )
+	gbeats = generate( model, quantize_resoluzion * 4 )
+	gtrack = beats2track( gbeats, 120 )
 
 	gmidi = midi.Pattern(
 		format = original.format,
@@ -43,3 +41,5 @@ if __name__ == '__main__':
 		tracks =  [ original[ 0 ], qtrack ] if ntracks == 2 else [ gtrack ]
 	)
 	midi.write_midifile( 'g_' + filename, gmidi )
+
+	writetables( [ beats2table( beats ), beats2table( gbeats ) ], filename + '.html' )
